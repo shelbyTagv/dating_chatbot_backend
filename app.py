7fbc7e9f9246a4ea9f7314d42717044136ce3d19
shelbyTagv <h220218p@hit.ac.zw>
2025-12-22 12:41:53 +0200

    new files removed venv

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ def create_pesepay_payment(uid: int, phone: str, method: str):
             return False
 
         payment = pesepay.create_payment(
-            "ZWL",
+            "ZWG",
             method,
             "noreply@shelbydates.com",
             clean_num,
